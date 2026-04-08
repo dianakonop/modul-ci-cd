bc7bf0e9e9c5abdcefca95cfc0754d0e2eb25926
dianakonop <dianakonoplanik12@gmail.com>
2026-04-08 18:59:46 +0300

    feat: add find_common_lines function

--- compare.py
+++ compare.py
@@ -1,3 +1,7 @@
 def read_file_lines(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         return set(line.strip() for line in f)
+
+def find_common_lines(lines1, lines2):
+    return lines1 & lines2
+
