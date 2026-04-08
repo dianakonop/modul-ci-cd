01707849b49128fa6373c5b70f0a3d5bf5033dd4
dianakonop <dianakonoplanik12@gmail.com>
2026-04-08 19:00:11 +0300

    feat: add write_lines function

--- compare.py
+++ compare.py
@@ -8,3 +8,8 @@ def find_common_lines(lines1, lines2):
 def find_diff_lines(lines1, lines2):
     return lines1 ^ lines2
 
+def write_lines(filename, lines):
+    with open(filename, 'w', encoding='utf-8') as f:
+        for line in sorted(lines):
+            f.write(line + '\n')
+
